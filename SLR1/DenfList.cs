8177c637de7f948cddf87b740ef29721b9fa4bbd
Luke <mizukiluke@outlook.com>
2018-06-06 15:47:16 +0800

    defination

---
+++ SLR1/DenfList.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SLR1.TypeList;
+
+namespace SLR1
+{
+    public class Pair<T, U>
+    {
+        public Pair()
+        {
+        }
+
+        public Pair(T first, U second)
+        {
+            this.First = first;
+            this.Second = second;
+        }
+
+        public T First { get; set; }
+        public U Second { get; set; }
+    };
+    public class DenfList
+    {
+        public List<Pair<String,DenfItem> > list;
+        public DenfList()
+        {
+            list = new List<Pair<String, DenfItem> >();
+        }
+        public bool isRepeat(String name)
+        {
+            foreach(var item in list)
+            {
+                if (item.First == name) return true;
+            }
+            return false;
+        }
+    }
+    public abstract class DenfItem
+    {
+        public String tokenName;
+        public int line;
+        public TypePtr typePtr;
+        public String kind;
+        public DenfItem(String tokenName,TypePtr typePtr,String kind,int line)
+        {
+            this.tokenName = tokenName;
+            this.typePtr = typePtr;
+            this.kind = kind;
+            this.line = line;
+        }
+    }
+    public class typeKind:DenfItem
+    {
+        public typeKind(String tokenName, TypePtr typePtr, int line) : base(tokenName, typePtr, "typeKind", line) { }
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(tokenName);
+            builder.Append("\t->\t");
+            builder.Append(typePtr.typeName + "\t");
+            builder.Append(kind);
+            return builder.ToString();
+        }
+    }
+    public enum Access { dir, indir };
+    public class varKind:DenfItem
+    {
+        Access access;
+        int level;
+        int off;
+        public varKind(String tokenName, TypePtr typePtr,Access access,int level,int off, int line) :base(tokenName, typePtr,"varKind", line)
+        {
+            this.access = access;
+            this.level = level;
+            this.off = off;
+        }
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(tokenName);
+            builder.Append("\t->\t");
+            builder.Append(typePtr.typeName + "\t");
+            builder.Append(kind+"\t");
+            builder.Append(access+"\t");
+            builder.Append(level + "\t");
+            builder.Append(off);
+            return builder.ToString();
+        }
+    }
+    public class fieldKind:DenfItem //是在struct里的变量，hostType是外部struct
+    {
+        int off;
+        TypePtr hostType;
+        public fieldKind(String tokenName, RecordTypePtr typePtr,int off, int line) :base(tokenName, typePtr,"fieldKind", line)
+        {
+            this.off = off;
+            this.hostType = typePtr;
+        }
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(tokenName);
+            builder.Append("\t->\t");
+            builder.Append(typePtr.typeName + "\t");
+            builder.Append(kind + "\t");
+            builder.Append(hostType.ToString());
+            return builder.ToString();
+        }
+    }
+    public enum ClassType { actual,formal}
+    public class actualRouteKind : DenfItem
+    {
+        int level;
+        ClassType classType;
+        public int parm=-1;
+        public int code;
+        public int size;
+        public bool forward;
+        public actualRouteKind(String tokenName, TypePtr typePtr,int level, int line) : base(tokenName, typePtr, "actualRouteKind", line)
+        {
+            this.level = level;
+            classType = ClassType.actual;
+            forward = false;
+        }
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(tokenName);
+            builder.Append("\t->\t");
+            builder.Append(typePtr.typeName + "\t");
+            builder.Append(kind + "\t");
+            builder.Append(level + "\t");
+            builder.Append(classType+"\t");
+            builder.Append(parm+"\t");
+            builder.Append("Leave a blank\t");
+            builder.Append("Leave a blank\t");
+            builder.Append(forward);
+            return builder.ToString();
+        }
+    }
+    public class formalRouteKind:DenfItem
+    {
+        int level;
+        ClassType classType;
+        int off;
+        public formalRouteKind(String tokenName, TypePtr typePtr, int level,int off, int line) : base(tokenName, typePtr, "actualRouteKind",line)
+        {
+            this.level = level;
+            classType = ClassType.formal;
+            this.off = off;
+        }
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(tokenName);
+            builder.Append("\t->\t");
+            builder.Append(typePtr.typeName + "\t");
+            builder.Append(kind + "\t");
+            builder.Append(level + "\t");
+            builder.Append(classType+"\t");
+            builder.Append(off);
+            return builder.ToString();
+        }
+    }
+}
--- SLR1/TypeList.cs
+++ SLR1/TypeList.cs
@@ -8,15 +8,39 @@ namespace SLR1
 {
     public class TypeList
     {
+        public List<TypePtr> list;
+        public TypeList()
+        {
+            list = new List<TypePtr>();
+            list.Add(new TypePtr("int"));
+            list.Add(new TypePtr("char"));
+            list.Add(new TypePtr("float"));
+            list.Add(new TypePtr("void"));
+        }
+        public TypePtr FindType(String kindStr)
+        {
+            foreach(var item in list)
+            {
+                if(item.typeName==kindStr)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        //基础数据类型的typeKind直接写typeName，其他则调用TypePtr参数
         public class TypePtr
         {
             public int size;
             public String typeName;
-
             public TypePtr(String typeName)
             {
                 size = 1;
                 this.typeName = typeName;
+            }
+            public override string ToString()
+            {
+                return size + "\t" + typeName;
             }
         }
         public class EnumTypePtr:TypePtr
@@ -39,19 +63,54 @@ namespace SLR1
                 size = length * elementTy.size;
             }
         }
-        public class StructTypePtr : TypePtr
+        public class RecordTypePtr : TypePtr
         {
-            InnerExpress express;
-            public StructTypePtr(InnerExpress express) : base("Struct")
+            public class RecordBody
             {
-                this.express = express;
-                size = express.size;
+                String name;
+                public TypePtr typePtr;
+                int offset;
+                
+                public RecordBody(String name,TypePtr typePtr,int offset)
+                {
+                    this.name = name;
+                    this.typePtr = typePtr;
+                    this.offset = offset;
+                }
+                public override string ToString()
+                {
+                    StringBuilder builder = new StringBuilder();
+                    builder.Append(name+"\t");
+                    builder.Append(typePtr.typeName + "\t");
+                    builder.Append(offset);
+                    return builder.ToString();
+                }
+            }
+            public void AddBody(RecordBody body)
+            {
+                records.Add(body);
+                size += body.typePtr.size;
+            }
+            public List<RecordBody> records;
+            public RecordTypePtr(String structName) : base(structName)
+            {
+                records = new List<RecordBody>();
+                size = 0;
+            }
+            public override string ToString()
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append("" + size + "\t" + typeName + "\n");
+                foreach(var item in records)
+                {
+                    builder.Append("\t");
+                    builder.Append(item.ToString());
+                    builder.Append("\n");
+                }
+                return builder.ToString();
             }
         }
-        static TypePtr IntPtr=new TypePtr("Int");
-        static TypePtr CharPtr = new TypePtr("Char");
-        static TypePtr FloatPtr = new TypePtr("Float");
-
+        
         public class InnerExpress
         {
             List<Express> list;
@@ -60,10 +119,20 @@ namespace SLR1
             {
                 list = new List<Express>();
             }
-            public void 
-                Add(Express express)
+            public void Add(Express express)
             {
                 list.Add(express);
+            }
+            public override String ToString()
+            {
+                StringBuilder builder = new StringBuilder();
+                foreach(var item in list)
+                {
+                    builder.Append("\t");
+                    builder.Append(item.ToString());
+                    builder.Append("\n");
+                }
+                return builder.ToString();
             }
         }
         public class Express
@@ -74,6 +143,10 @@ namespace SLR1
             {
                 this.type = type;
                 this.iden = iden;
+            }
+            public override String ToString()
+            {
+                return "" + type.typeName + " " + iden;
             }
         }
     }
